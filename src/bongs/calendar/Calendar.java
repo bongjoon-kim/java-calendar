be9df11d3a55466feb283bbb95a09764344bd3b3
Administrator <Administrator@system-kbj3>
2019-07-03 09:04:59 +0900

    Month name convert

--- src/bongs/calendar/Calendar.java
+++ src/bongs/calendar/Calendar.java
@@ -4,8 +4,8 @@ public class Calendar {
 
 	public static void main(String[] args) {
 		
-		System.out.println("일 월 화 수 목 금 토");
-		System.out.println("-------------------");
+		System.out.println("SU MO TU WE TH FR SA");
+		System.out.println("--------------------");
 		System.out.println("01 02 03 04 05 06 07");
 		System.out.println("08 09 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
