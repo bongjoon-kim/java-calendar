70cf39493609fb905931dd0a846b0ffaf73951f7
Administrator <Administrator@system-kbj3>
2019-07-03 18:48:47 +0900

    가상의 달력

--- src/bongs/calendar/Calendar.java
+++ src/bongs/calendar/Calendar.java
@@ -6,19 +6,35 @@ public class Calendar {
 	
 	public static final int[] MAXDAY_OF_MONTH = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};		
 	
-	public void sampleCalendar() {
+	public void sampleCalendar(int maxDayOfMonth) {
 		System.out.println("SU MO TU WE TH FR SA");
 		System.out.println("--------------------");
-		System.out.println("01 02 03 04 05 06 07");
-		System.out.println("08 09 10 11 12 13 14");
-		System.out.println("15 16 17 18 19 20 21");
-		System.out.println("22 23 24 25 26 27 28");		
+		
+		for (int i = 1; i <= maxDayOfMonth; i++) {
+			if (i < 10) {
+				System.out.print(" 0" + i);
+			} else {
+				System.out.print(" " + i);
+			}
+			
+			if (i % 7 == 0) {
+				System.out.println();
+			}
+		}
+		System.out.println();
 	}
 	
 	public int getMaxdayOfMonth(int month) {
 		return MAXDAY_OF_MONTH[month - 1];
 	}
 
+	/*
+	* 월을 입력하면 해당월의 달력을 출력한다.
+	* 달력의 모양은 1단계에서 작성한 모양으로 만든다.
+	* 1일은 일요일로 정해도 무방하다.
+	* -1을 입력받기 전까지 반복 입력받는다.
+	* 프롬프트를 출력한다.
+	*/
 	public static void main(String[] args) {
 
 		// 숫자를 입력받아 해당하는 달의 최대일수를 출력하는 프로그램
@@ -26,7 +42,7 @@ public class Calendar {
 		Calendar cal = new Calendar();
 		
 		while (true) {
-			System.out.println("달을 입력하세요.");
+			System.out.println("월을 입력하세요.");
 			System.out.print("cal> ");
 			int month = sc.nextInt();
 			
@@ -38,11 +54,11 @@ public class Calendar {
 				continue;
 			}			
 				
-			System.out.printf("%d월의 마지막 날은 %d일 입니다.\n\n", month, cal.getMaxdayOfMonth(month));
+			cal.sampleCalendar(cal.getMaxdayOfMonth(month));
 		}
 		
 		System.out.println("Bye~");
-		//cal.sampleCalendar();
+		
 		sc.close();
 	}
 
